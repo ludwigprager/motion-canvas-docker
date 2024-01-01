801bfa8b3712f34b62ef4d3e3d79d5867c8689fe
Ludwig Prager <ludwig.prager@celp.de>
2024-01-01 19:06:21 +0100

    s6 layout scene

--- projects/p6/src/project.ts
+++ projects/p6/src/project.ts
@@ -4,12 +4,13 @@ import example from './scenes/example?scene';
 import example2 from './scenes/example2?scene';
 import s3 from './scenes/s3?scene';
 import s4 from './scenes/s4?scene';
+import s5 from './scenes/s5?scene';
 import layouts from './scenes/layouts?scene';
 // ...
 import audio from '../audio/voice.mp3';
 
 export default makeProject({
-  scenes: [example, example2, s3, s4],
+  scenes: [example, example2, s3, s4, s5],
 //scenes: [ s4],
   audio: audio,
 });
